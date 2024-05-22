fd1f2b241a596bc01fc366d87891c364d125afe4
phamquyquoc <phamyeunuoc@gmail.com>
2024-05-22 10:43:53 +0700

    ok

---
+++ View/server.js
@@ -0,0 +1,31 @@
+const mysql2 = require("mysql2");
+const app = require("./app");
+app.set('view engine','ejs');
+app.set('VIEW','./VIEW');
+app.get('/',(req,res)=>{
+    res.render('searchProduct');
+})
+app.listen(3000, () => {
+  console.log("isten on 3001...");
+});
+// exports.connect = () => {
+//   const connect = mysql2.createConnection({
+//     host: "localhost",
+//     port: "3306",
+//     user: "root",
+//     password: "phuc1234",
+//     database: "ql",
+//   });
+//   connect.connect((err, connect) => {
+//     if (!err) {
+//       console.log("seceedd..");
+//     } else {
+//       console.log(err);
+//     }
+//   });
+//   return connect;
+// };
+// connect.query("SELECT * FROM ql.book;", (err, results) => {
+//   console.log(results);
+// });
+// module.exports = connect;
